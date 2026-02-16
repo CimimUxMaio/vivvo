459a995ae468a7d5d5604042985f4da4eb9e2469
CimimUxMaio <juanbiabdon@gmail.com>
2026-02-16 19:35:36 -0300

    Add format helpers for currency and time formatting

--- lib/vivvo_web/helpers/format_helpers.ex
+++ lib/vivvo_web/helpers/format_helpers.ex
@@ -75,4 +75,78 @@ defmodule VivvoWeb.FormatHelpers do
         format_datetime(datetime)
     end
   end
+
+  @doc """
+  Formats the number of days until a date into a human-friendly string.
+
+  ## Examples
+
+      iex> format_time_until(5)
+      "in 5 days"
+
+      iex> format_time_until(1)
+      "tomorrow"
+
+      iex> format_time_until(0)
+      "today"
+
+      iex> format_time_until(-2)
+      "2 days ago"
+
+      iex> format_time_until(nil)
+      ""
+  """
+  def format_time_until(nil), do: ""
+
+  def format_time_until(days) when is_integer(days) do
+    cond do
+      days < 0 -> "#{days * -1} days ago"
+      days == 0 -> "today"
+      days == 1 -> "tomorrow"
+      true -> "in #{days} days"
+    end
+  end
+
+  @doc """
+  Formats a monetary amount as USD currency.
+
+  ## Examples
+
+      iex> format_currency(Decimal.new("1234.56"))
+      "$1,234.56"
+
+      iex> format_currency(100)
+      "$100.00"
+
+      iex> format_currency(1234.56)
+      "$1,234.56"
+
+      iex> format_currency(nil)
+      "$0.00"
+  """
+  def format_currency(nil), do: "$0.00"
+
+  def format_currency(amount) when is_struct(amount, Decimal) do
+    amount
+    |> Decimal.to_float()
+    |> format_currency()
+  end
+
+  def format_currency(amount) when is_float(amount) or is_integer(amount) do
+    # Convert to float and format with 2 decimal places
+    formatted = :erlang.float_to_binary(amount * 1.0, decimals: 2)
+
+    # Add thousands separator
+    [dollars, cents] = String.split(formatted, ".")
+
+    dollars_with_commas =
+      dollars
+      |> String.graphemes()
+      |> Enum.reverse()
+      |> Enum.chunk_every(3)
+      |> Enum.map_join(",", &Enum.join(&1, ""))
+      |> String.reverse()
+
+    "$#{dollars_with_commas}.#{cents}"
+  end
 end
