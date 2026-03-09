6c7f8096e78db27427720d4ef2ee302a526b2057
CimimUxMaio <juanbiabdon@gmail.com>
2026-03-08 21:07:58 -0300

    Create rent periods table with indexes and constraints

---
+++ priv/repo/migrations/20260309000151_create_rent_periods.exs
@@ -0,0 +1,19 @@
+defmodule Vivvo.Repo.Migrations.CreateRentPeriods do
+  use Ecto.Migration
+
+  def change do
+    create table(:rent_periods) do
+      add :value, :decimal, null: false
+      add :index_type, :string, null: true
+      add :index_value, :decimal, null: true
+      add :start_date, :date, null: false
+      add :end_date, :date, null: false
+      add :contract_id, references(:contracts, on_delete: :delete_all), null: false
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:rent_periods, [:contract_id])
+    create index(:rent_periods, [:contract_id, :start_date, :end_date])
+  end
+end
